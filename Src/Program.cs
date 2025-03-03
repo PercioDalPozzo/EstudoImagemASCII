c1a279a7497288b6c965b2a093a9855b2511f83a
Pércio Ebsen Dal Pozzo <percio.pozzo@softplan.com.br>
2025-03-03 16:31:06 -0300

    Ajustes

--- Src/Program.cs
+++ Src/Program.cs
@@ -7,19 +7,16 @@ class Program
 {
     static void Main()
     {
-        string imagePath = "../../../../eu.jpg";
-        string pathDestino = "../../../../";
+        var caminhoImagem = "../../../../eu.jpg";
+        var caminhoDestino = "../../../../";
+        const int resolucao = 100;
         
-        const int resolucao = 50;
-        //const int chars = 2;
-        
-        using (var image = Image.Load<Rgba32>(imagePath))
+        using (var image = Image.Load<Rgba32>(caminhoImagem))
         {
-            //Converter(image, chars, resolucao, pathDestino);
-            Converter(image, 2, resolucao, pathDestino);
-            Converter(image, 5, resolucao, pathDestino);
-            Converter(image, 10, resolucao, pathDestino);
-            Converter(image, 20, resolucao, pathDestino);
+            Converter(image, 2, resolucao, caminhoDestino);
+            Converter(image, 5, resolucao, caminhoDestino);
+            Converter(image, 10, resolucao, caminhoDestino);
+            Converter(image, 20, resolucao, caminhoDestino);
         }
        
         Console.WriteLine("Feito!");
@@ -28,11 +25,7 @@ class Program
     private static void Converter(Image<Rgba32> image, int chars, int resolucao, string pathDestino)
     {
         var builder = new ImageConverter(image).Convert(new PixelConverter(new Luminosidade_R21_G71_B01(),chars), resolucao);
-        File.WriteAllText($"{pathDestino}Imagem1 chars{chars} resolucao{resolucao} - {DateTime.Now.ToString("hh_mm_ss")}.txt", builder.ToString());
-        
-        var builder2 = new ImageConverter(image).Convert(new PixelConverter(new Luminosidade_R03_G59_B11(),chars), resolucao);
-        File.WriteAllText($"{pathDestino}Imagem2 chars{chars} resolucao{resolucao} - {DateTime.Now.ToString("hh_mm_ss")}.txt", builder2.ToString());
-        
+        File.WriteAllText($"{pathDestino}Imagem chars{chars} resolucao{resolucao} - {DateTime.Now.ToString("hh_mm_ss")}.txt", builder.ToString());
     }
 }
 
